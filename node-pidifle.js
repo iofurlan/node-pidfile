90ad019e5be760849be8dc5643362af9725f9719
iofurlan <dario.furlan.51@gmail.com>
2018-09-20 22:47:36 +0200

    updates

--- node-pidifle.js
+++ node-pidifle.js
@@ -1,27 +1,26 @@
-var fs = require('fs');
+"use strict";
+const fs = require('fs');
 
 function start(pidfile, exec) {
-	fs.writeSync(pidfile, process.pid);
-	exec();
-	fs.unlinkSync(pidfile);
+    fs.writeFileSync(pidfile, process.pid);
+    exec();
+    fs.unlinkSync(pidfile);
 }
 
 function with_pid(filename, exec) {
-	const pidfile = filename+".pid"
-	if (fs.existsSync(pidfile)) {
-		let existing_pid = fs.readFileSync(pidfile, 'utf8');
-		try {
-			process.kill(existing_pid, 0);
-			console.error("Already Running, exiting...");
-			process.exit(0);
-		} catch(e) {
-			start(pidifle, exec);
-		}
-	} else {
-		start(pidifle, exec);
-	}
+    const pidfile = filename + ".pid";
+    if (fs.existsSync(pidfile)) {
+        let existing_pid = fs.readFileSync(pidfile, 'utf8');
+        try {
+            process.kill(existing_pid, "0");
+            console.error("Already Running, exiting...");
+            process.exit(0);
+        } catch (e) {
+            start(pidfile, exec);
+        }
+    } else {
+        start(pidfile, exec);
+    }
 }
 
-module.export = {
-	with_pid: with_pid
-}
+module.exports = with_pid;
